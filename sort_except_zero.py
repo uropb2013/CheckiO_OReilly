9b7411609ef36be82e7fea9b7a5b924f3da77b1a
Шелохин Игорь <uropb2013@yandex.ru>
2022-01-24 18:12:04 +0300

    sort_except_zero complete

---
+++ sort_except_zero.py
@@ -0,0 +1,27 @@
+from typing import Iterable
+
+
+def except_zero(items: list) -> Iterable:
+    zeros = []
+    for i in range(len(items)):
+        if items[i] == 0:
+            zeros.append(i)
+    while items.count(0):
+        items.remove(0)
+    items.sort()
+    for i in range(len(zeros)):
+        items.insert(zeros[i], 0)
+    return items
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])) == [1, 3, 0, 0, 4, 4, 5, 0, 7]
+    assert list(except_zero([0, 2, 3, 1, 0, 4, 5])) == [0, 1, 2, 3, 0, 4, 5]
+    assert list(except_zero([0, 0, 0, 1, 0])) == [0, 0, 0, 1, 0]
+    assert list(except_zero([4, 5, 3, 1, 1])) == [1, 1, 3, 4, 5]
+    assert list(except_zero([0, 0])) == [0, 0]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
