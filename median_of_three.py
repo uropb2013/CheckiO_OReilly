dfcd725934a2830b8f490e2a9df638cda8a88b20
Шелохин Игорь <uropb2013@yandex.ru>
2022-01-26 15:36:02 +0300

    median_of_three complete

--- median_of_three.py
+++ median_of_three.py
@@ -22,6 +22,6 @@ if __name__ == '__main__':
     print(list(median_three([5, 2, 9, 1, 7, 4, 6, 3, 8])))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert list(median_three([5, 2, 9, 1, 7, 4, 6, 3, 8])) == [5, 2, 5, 2, 7, 4, 6, 4, 6]
+    assert list(median_three([1, 2, 3, 4, 5, 6, 7])) == [1, 2, 2, 3, 4, 5, 6]
     assert list(median_three([1])) == [1]
     print("Coding complete? Click 'Check' to earn cool rewards!")
